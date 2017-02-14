d99e9e9554ac6d358a8296a565a0eb6741e218bf
HaJohnathan <ha.johnathan97@gmail.com>
2017-02-14 01:35:08 -0500

    Add files via upload

--- week1/js/sketch.js
+++ week1/js/sketch.js
@@ -1,12 +1,15 @@
 function setup() {
     createCanvas(720, 540);
     background('lightblue');
-    ellipse(300, 300, 500);
+    fill('#f5c297')
+    arc(100, 230, 150, 150, HALF_PI, PI + 1.57)
+    arc(500, 230, 150, 150, PI + 1.57, HALF_PI)
+    ellipse(300, 300, 500)
+    triangle(300, 275, 325, 325, 275, 325)
+    arc(300, 280, 250, 250, 0 + 1, PI - 1);
+    fill('white')
     ellipse(200, 200, 65, 55);
     ellipse(400, 200, 65, 55);
-    triangle(300, 275, 325, 325, 275, 325)
-    stroke('pink')
-    arc(300, 280, 250, 250, 0 + 1, PI - 1);
     stroke('purple')
     fill('red')
     ellipse(200, 200, 45, 50)
@@ -15,4 +18,8 @@ function setup() {
     fill('black')
     ellipse(200, 200, 5, 50)
     ellipse(400, 200, 5, 50)
+    stroke('black')
+    arc(200, 160, 65, 15, PI, TWO_PI);
+    arc(400, 160, 65, 15, PI, TWO_PI);
+    arc(300, 120, 350, 150, PI, TWO_PI);
 }
