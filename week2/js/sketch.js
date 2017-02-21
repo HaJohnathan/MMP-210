84eac6955fc374cae70f0fa945bd727f4cc773bf
HaJohnathan <ha.johnathan97@gmail.com>
2017-02-21 02:17:33 -0500

    Add files via upload

--- week2/js/sketch.js
+++ week2/js/sketch.js
@@ -16,27 +16,500 @@ function setup() {
     var ncy = 120;
     var hcx = 54;
     var hcy = 60;
-    var dia = quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    var resetx = 432
+    var resety = 600
+    var resetx2 = 540
+    var resety2 = 720
+    var btlx = 0
+    var btly = 0
+    var btrx = 540
+    var btry = 0
+    var bbrx = 540
+    var bbry = 30
+    var bblx = 0
+    var bbly = 30
+    var stlx = 0
+    var stly = 0
+    var strx = 13.5
+    var stry = 0
+    var sbrx = 13.5
+    var sbry = 720
+    var sblx = 0
+    var sbly = 720
+    var bc = 690
+    var sc = 526.5
+    var cent1 = 270
+    var cent2 = 360
+    var hp = HALF_PI
+    var thp = PI + HALF_PI
+    var aw = 400
+    var ah = 400
+    var ac1 = 50
+    var ac2 = 150
+    var ac3 = 280
+    var cc = 60
+    var elx = 225
+    var elxc = 85
+    var ely = 300
+    var elw = 50
+    var elh = 30
     createCanvas (540, 720);
     background (db);
     fill(mb);
     quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
     dlx += ncx;
-    dly += ncy;
     dtx += ncx;
-    dty += ncy;
     dbx += ncx;
-    dby += ncy;
     drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx;
+    dtx -= resetx;
+    dbx -= resetx;
+    drx -= resetx;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
     dry += ncy;
     quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
     dlx += ncx;
-    dly += ncy;
     dtx += ncx;
-    dty += ncy;
     dbx += ncx;
-    dby += ncy;
     drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx;
+    dtx -= resetx;
+    dbx -= resetx;
+    drx -= resetx;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
     dry += ncy;
-    dia;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx;
+    dtx -= resetx;
+    dbx -= resetx;
+    drx -= resetx;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
+    dry += ncy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx;
+    dtx -= resetx;
+    dbx -= resetx;
+    drx -= resetx;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
+    dry += ncy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx;
+    dtx -= resetx;
+    dbx -= resetx;
+    drx -= resetx;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
+    dry += ncy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx;
+    dtx -= resetx;
+    dbx -= resetx;
+    drx -= resetx;
+    dly -= resety;
+    dty -= resety;
+    dby -= resety;
+    dry -= resety;
+    fill(lb);
+    dlx -= hcx;
+    dtx -= hcx;
+    dbx -= hcx;
+    drx -= hcx;
+    dly -= hcy;
+    dty -= hcy;
+    dby -= hcy;
+    dry -= hcy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx2;
+    dtx -= resetx2;
+    dbx -= resetx2;
+    drx -= resetx2;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
+    dry += ncy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx2;
+    dtx -= resetx2;
+    dbx -= resetx2;
+    drx -= resetx2;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
+    dry += ncy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx2;
+    dtx -= resetx2;
+    dbx -= resetx2;
+    drx -= resetx2;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
+    dry += ncy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx2;
+    dtx -= resetx2;
+    dbx -= resetx2;
+    drx -= resetx2;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
+    dry += ncy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx2;
+    dtx -= resetx2;
+    dbx -= resetx2;
+    drx -= resetx2;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
+    dry += ncy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx -= resetx2;
+    dtx -= resetx2;
+    dbx -= resetx2;
+    drx -= resetx2;
+    dly += ncy;
+    dty += ncy;
+    dby += ncy;
+    dry += ncy;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    dlx += ncx;
+    dtx += ncx;
+    dbx += ncx;
+    drx += ncx;
+    quad(dlx, dly, dtx, dty, drx, dry, dbx, dby);
+    noStroke();
+    fill(db);
+    quad(btlx, btly, btrx, btry, bbrx, bbry, bblx, bbly);
+    btly += bc;
+    btry += bc;
+    bbry += bc;
+    bbly += bc;
+    quad(btlx, btly, btrx, btry, bbrx, bbry, bblx, bbly);
+    quad(stlx, stly, strx, stry, sbrx, sbry, sblx, sbly);
+    stlx += sc;
+    strx += sc;
+    sbrx += sc;
+    sblx += sc;
+    quad(stlx, stly, strx, stry, sbrx, sbry, sblx, sbly);
+    fill(w)
+    arc(cent1, cent2, aw, ah, hp, thp);
+    fill(bl)
+    arc(cent1, cent2, aw, ah, thp, hp);
+    aw -= ac1
+    ah -= ac1
+    arc(cent1, cent2, aw, ah, hp, thp);
+    fill(w)
+    arc(cent1, cent2, aw, ah, thp, hp);
+    aw -= ac2
+    ah -= ac1
+    arc(cent1, cent2, aw, ah, hp, thp);
+    fill(bl)
+    arc(cent1, cent2, aw, ah, thp, hp);
+    cent2 += cc
+    aw -= ac2
+    ah -= ac3
+    arc(cent1, cent2, aw, ah, hp, thp);
+    ellipse( elx, ely, elw, elh)
+    fill(w)
+    elx += elxc
+    arc(cent1, cent2, aw, ah, thp, hp);
+    ellipse( elx, ely, elw, elh)
 }
